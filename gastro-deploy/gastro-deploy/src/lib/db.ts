3987d208c640a74393d6519c4b0bd041f89718b5
abdalllah7-beep <abdalllah7@gmail.com>
2026-03-13 03:22:06 +0200

    Delete gastro-deploy/gastro-deploy directory

--- gastro-deploy/gastro-deploy/src/lib/db.ts
+++
@@ -1,5 +0,0 @@
-// Simple in-memory storage for serverless deployment
-// No database required - all data stored in session/localStorage on client
-
-// This file exists for compatibility but doesn't use any database
-export const db = null;
